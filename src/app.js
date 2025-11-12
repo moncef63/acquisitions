4e851734af4608c0cd1b6da84a4fcc96dee85ba4
moncef63 <moncif.ayouch@gmail.com>
2025-11-12 03:04:10 +0100

    Implent ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import express from "express";
+import express from 'express';
 
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('hello from Acquisitions!');
-})
+  res.status(200).send('hello from Acquisitions!');
+});
 
 export default app;
